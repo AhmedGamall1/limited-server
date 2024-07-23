c40738175f0e6b475f787735547834fce275a535
ahmed <ag3204123@gmail.com>
2024-07-23 02:48:55 -0700

    fix auth bug

--- utils/token/createToken.js
+++ utils/token/createToken.js
@@ -9,7 +9,7 @@ export const createToken = (user, statusCode, res) => {
   res.cookie("jwt", token, {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
+    sameSite: "lax",
     secure: process.env.NODE_ENV !== "development",
   });
 
